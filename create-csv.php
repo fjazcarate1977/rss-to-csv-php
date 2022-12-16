61e2f755b98c14c597dc6ee7ff5b49b3590c02bd
Francisco Javier Azcárate de Castro <pacoazcarate@gmail.com>
2022-12-16 07:27:32 +0100

    New field stock level added

--- create-csv.php
+++ create-csv.php
@@ -1,52 +1,84 @@
 <?php
 
-    $rss_tags = array(
-        'id',
-        'price',
-        'availability',
-    );
-    $rss_item_tag = 'item';
-    $rss_url = 'https://www.fricosmos.com/google-merchant/google_merchant_es.xml';
-    $rss_csv = 'fricosmos.csv';
+$rss_tags = array(
+    'id',
+    'price',
+    'availability',
+    'custom_label_0'
+);
+$rss_item_tag = 'item';
+$rss_url = 'https://www.fricosmos.com/google-merchant/google_merchant_es.xml';
+$rss_csv = 'fricosmos.csv';
 
-    $current_date_time = date('h:i:sa') . ' ' . date('Y/m/d');
+$columns_names = array(
+    'REF1',
+    'PRECIO',
+    'STOCK',
+    'NIVELSTOCK',
+    'REF2',
+);
 
-    function rss_to_array($tag, $array, $url) {
-        $doc = new DOMdocument();
-        $doc->load($url);
-        $rss_array = array();
-        $items = array();
-        foreach($doc->getElementsByTagName($tag) AS $node) {    
-            foreach($array AS $key => $value) {
-                $val = $node->getElementsByTagName($value)->item(0)->nodeValue;
-                if($value == 'availability') {
-                    $items[$value] = $val == 'in stock' ? 0 : 1;
-                } elseif ($value == 'price') {
-                    $newval= str_replace(' EUR', '', $val);
-                    $items[$value] = number_format($newval, 2, ',', '.');
-                } else {
-                    $items[$value] = $val;
-                }    
-   
+$current_date_time = date('h:i:sa') . ' ' . date('Y/m/d');
+
+function parser_stock_level($val)
+{
+    switch ($val) {
+        case "Sin stock":
+            return 0;
+            break;
+        case "Nivel de stock: escaso":
+            return 1;
+            break;
+        case "Nivel de stock: moderado":
+            return 2;
+            break;
+        case "Nivel de stock: amplio":
+            return 3;
+            break;
+        default:
+            return 0;
+    }
+}
+
+function rss_to_array($tag, $array, $url)
+{
+    $doc = new DOMdocument();
+    $doc->load($url);
+    $rss_array = array();
+    $items = array();
+    foreach ($doc->getElementsByTagName($tag) as $node) {
+        foreach ($array as $key => $value) {
+            $val = $node->getElementsByTagName($value)->item(0)->nodeValue;
+            if ($value == 'availability') {
+                $items[$value] = $val == 'in stock' ? 0 : 1;
+            } elseif ($value == 'price') {
+                $newval = str_replace(' EUR', '', $val);
+                $items[$value] = number_format($newval, 2, ',', '');
+            } elseif ($value == 'custom_label_0') {
+                $items[$value] = parser_stock_level($val);
+            } else {
+                $items[$value] = $val;
             }
-            array_push($rss_array, $items);
         }
-        return $rss_array;
+        array_push($rss_array, $items);
     }
+    return $rss_array;
+}
 
-    echo 'Fricosmos import XML has started at ' . $current_date_time . PHP_EOL ;
+echo 'Fricosmos import XML has started at ' . $current_date_time . PHP_EOL;
 
-    $rssfeed = rss_to_array($rss_item_tag,$rss_tags,$rss_url);
+$rssfeed = rss_to_array($rss_item_tag, $rss_tags, $rss_url);
 
-    foreach ($rssfeed as &$val) $val['idtag'] = "FRI" . $val['id'];
+foreach ($rssfeed as &$val) $val['idtag'] = "FRI" . $val['id'];
 
-    $fp = fopen($rss_csv, 'w');
-    foreach ($rssfeed as $fields) {
-        fputcsv($fp, $fields, ';');
-    }
-  
-    fclose($fp);
+$fp = fopen($rss_csv, 'w');
 
-    echo 'Fricosmos import XML has finished at ' . $current_date_time . PHP_EOL . 'Please check the above log for more information.' . PHP_EOL;
+fputcsv($fp, $columns_names, ';');
 
-?>
+foreach ($rssfeed as $fields) {
+    fputcsv($fp, $fields, ';');
+}
+
+fclose($fp);
+
+echo 'Fricosmos import XML has finished at ' . $current_date_time . PHP_EOL . 'Please check the above log for more information.' . PHP_EOL;
